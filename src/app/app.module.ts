df055a060d245c1c61714baee9192672b80355de
mwjkaki <kakiya@mwjapan.jp>
2019-04-01 16:02:23 +0900

    7 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { OrdHeadComponent } from './ordhead.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { 
+  MatButtonModule, 
+  MatCheckboxModule, 
+  MatDatepickerModule, 
+  MatNativeDateModule, 
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule ,BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule, 
+    MatNativeDateModule, 
+    MatInputModule
+   ],
+  declarations: [ AppComponent, OrdHeadComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
